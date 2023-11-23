35cbd22822d6b6f67317f7b3d94b3ee59066cf19
khj1121p <140360970+khj1121p@users.noreply.github.com>
2023-11-23 14:28:40 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -17,48 +17,49 @@ $(document).ready(function(){
     });
 
     $('.h-menu-head').click(function(){
-        $(this).siblings('.h-sub-menu').slideToggle();
+        $(this).siblings('.h-sub-menu').slideDown();
+        $(this).parents().siblings().children('.h-sub-menu').slideUp();
     });
 
     // // 스크롤
     var ww = $(window).width();
-    // if(ww > 1124){
-    //     $(window).scroll(function(){
-    //         const sct = $(window).scrollTop();
-    //         var wh = $(window).height();
-    //         console.log(sct);
-    //         var con1 = $('.wrap').height();
-    //         var ww = $(window).width();
-    //         var cw = $('.sec-3 .container').width();
-    //         console.log(sct);
-    //         const sec3 = $('.sec-3').offset().top;
-    //         const sec3h = $('.sec-3').height();
-    //         const sec4 = $('.sec-4').offset().top;
-    //         const sec4h = $('.sec-4').height();
-    //         const bh = $('.sec-3 .right').height();
+    if(ww > 1124){
+        $(window).scroll(function(){
+            const sct = $(window).scrollTop();
+            var wh = $(window).height();
+            console.log(sct);
+            var con1 = $('.wrap').height();
+            var ww = $(window).width();
+            var cw = $('.sec-3 .container').width();
+            console.log(sct);
+            const sec3 = $('.sec-3').offset().top;
+            const sec3h = $('.sec-3').height();
+            const sec4 = $('.sec-4').offset().top;
+            const sec4h = $('.sec-4').height();
+            const bh = $('.sec-3 .right').height();
 
-    //         if(sct >= sec3 && sct < sec4 - bh){
-    //             $('.sec-3 .right').css({
-    //                 position: 'fixed',
-    //                 top: 0,
-    //                 right: parseInt((ww - cw) / 2),
-    //             })
-    //         }else if(sct < sec3){
-    //             $('.sec-3 .right').css({
-    //                 position: 'static',
-    //             })
-    //         }else if(sct >= sec4 - bh){
-    //             $('.sec-3 .right').css({
-    //                 position: 'absolute',
-    //                 top: 'auto',
-    //                 right: 0,
-    //                 bottom: 36,
-    //             })
-    //         }
-    //         });
-    // }else{
+            if(sct >= sec3 && sct < sec4 - bh){
+                $('.sec-3 .right').css({
+                    position: 'fixed',
+                    top: 0,
+                    right: parseInt((ww - cw) / 2),
+                })
+            }else if(sct < sec3){
+                $('.sec-3 .right').css({
+                    position: 'static',
+                })
+            }else if(sct >= sec4 - bh){
+                $('.sec-3 .right').css({
+                    position: 'absolute',
+                    top: 'auto',
+                    right: 0,
+                    bottom: 36,
+                })
+            }
+            });
+    }else{
         
-    // }
+    }
 
     // footer sub-menu-box
     $('.f-menu-head').click(function(){
@@ -71,51 +72,51 @@ $(document).ready(function(){
     function layout(){
         var ww = $(window).width();
 
-        if(ww > 1124){
-            $(window).scroll(function(){
-                const sct = $(window).scrollTop();
-                var wh = $(window).height();
-                console.log(sct);
-                var con1 = $('.wrap').height();
-                var ww = $(window).width();
-                var cw = $('.sec-3 .container').width();
-                console.log(sct);
-                const sec3 = $('.sec-3').offset().top;
-                const sec3h = $('.sec-3').height();
-                const sec4 = $('.sec-4').offset().top;
-                const sec4h = $('.sec-4').height();
-                const bh = $('.sec-3 .right').height();
+        // if(ww > 1124){
+        //     $(window).scroll(function(){
+        //         const sct = $(window).scrollTop();
+        //         var wh = $(window).height();
+        //         console.log(sct);
+        //         var con1 = $('.wrap').height();
+        //         var ww = $(window).width();
+        //         var cw = $('.sec-3 .container').width();
+        //         console.log(sct);
+        //         const sec3 = $('.sec-3').offset().top;
+        //         const sec3h = $('.sec-3').height();
+        //         const sec4 = $('.sec-4').offset().top;
+        //         const sec4h = $('.sec-4').height();
+        //         const bh = $('.sec-3 .right').height();
     
-                if(sct >= sec3 && sct < sec4 - bh){
-                    $('.sec-3 .right').css({
-                        position: 'fixed',
-                        top: 0,
-                        right: parseInt((ww - cw) / 2),
-                    })
-                }else if(sct < sec3){
-                    $('.sec-3 .right').css({
-                        position: 'static',
-                    })
-                }else if(sct >= sec4 - bh){
-                    $('.sec-3 .right').css({
-                        position: 'absolute',
-                        top: 'auto',
-                        right: 0,
-                        bottom: 36,
-                    })
-                }
-                });
-        }else{
-            $(window).scroll(function(){
-                if(sct >= sec3 && sct < sec4){
-                    $('.sec-3 .right').css({
-                        positio: 'static'
-                    })
-                }else{
+        //         if(sct >= sec3 && sct < sec4 - bh){
+        //             $('.sec-3 .right').css({
+        //                 position: 'fixed',
+        //                 top: 0,
+        //                 right: parseInt((ww - cw) / 2),
+        //             })
+        //         }else if(sct < sec3){
+        //             $('.sec-3 .right').css({
+        //                 position: 'static',
+        //             })
+        //         }else if(sct >= sec4 - bh){
+        //             $('.sec-3 .right').css({
+        //                 position: 'absolute',
+        //                 top: 'auto',
+        //                 right: 0,
+        //                 bottom: 36,
+        //             })
+        //         }
+        //         });
+        // }else{
+        //     $(window).scroll(function(){
+        //         if(sct >= sec3 && sct < sec4){
+        //             $('.sec-3 .right').css({
+        //                 positio: 'static'
+        //             })
+        //         }else{
 
-                }
-            });
-        }
+        //         }
+        //     });
+        // }
 
         if(ww <= 1124){
             $('.sec-3 .right').insertBefore('.sec-3 .left');
